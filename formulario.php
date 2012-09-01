3437d19a0ddcaa459e7795f36cd070076132c060
Maximiliano Opitz <mopitz199@gmail.com>
2012-09-01 01:19:15 -0400

    tercer commit

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php include("clases/conectar-base-datos.php");?>
+
+
+	<p style="color:blue;">
+		<?php conectar(); ?>	
+	</p>
+</body>
+</html>
