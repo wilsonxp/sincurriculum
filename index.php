9aba00ffdb7a4af1e9b6504eccaad292b70744d8
root <root@erick-Inspiron-N7010.(none)>
2012-09-02 01:46:19 -0300

    pagina

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Sin Curriculum</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="estilos.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+		<header class="container">
+			<div id="logo">
+				<h1><a href="#">SinCurriculum</a></h1>
+				<p>Publica y encuentra <a href="">Portafolios</a></p>
+			</div>
+			<div id="menu">
+				<ul>
+					<li class="current_page_item"><a href="#">Inicio</a></li>
+					<li><a href="#">Portafolios</a></li>
+					<li><a href="registro.php">Registrarse</a></li>
+				</ul>
+			</div>
+		</header>
+		<section>
+			<article id="bienvenida">
+				<h2>¿Eres medico y siempre quisiste  ser pintor?</h2>
+				<p><strong>SinCurriculum</strong> es un plataforma donde eliminamos los curriculum y nos enfocamos en todas aquellas personas que sin titulo, sin universidad tienen conocimiento, proyectos e ideas. Aquí tú como, podrás generar clientes tener trabajo exponer tus proyectos aumentar las visitas a tu portafolio.</p>
+				<p>Tu como Usuario puedes encontrar una variedad de</p>	
+			</article>
+			<div id="linkregist"><a href="registro.php">Regístrate</a></div>
+			<div id="apurate"><p>Quedan 0212 cupos <strong>gratis</strong>... Apurate!</p></div>	
+		</section>		
+</div>
+	<footer>
+		<p>Copyright (c) 2012 - SinCurriculum.com All rights reserved.</p>
+	</footer>
+</body>
+</html>
---
+++ portafolios.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Sin Curriculum</title>
+	<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+	<link href="estilos.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div>
+	<header class="container">
+		<div id="logo">
+			<h1><a href="index.php">SinCurriculum</a></h1>
+			<p>Publica y encuentra <a href="">Portafolios</a></p>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#">Inicio</a></li>
+				<li><a href="#">Portafolios</a></li>
+				<li><a href="registro.php">Registrarse</a></li>
+			</ul>
+		</div>
+	</header>
+	<section id="publicaciones">
+		<article class="publicacion">
+			<p>Nombre<p>
+			<p>Email</p>
+			<p>Area</p>
+			<p>¿Que haces?</p>
+			<a>Mis trabajos</a>
+		</article>	
+	</section>
+	<sidebar>
+		<h3>Categorias</h3>
+		<p></p>
+	</sidebar>	
+</div>
+<footer>
+	<p>Copyright (c) 2012 - SinCurriculum.com All rights reserved.</p>
+</footer>
+</body>
+</html>
